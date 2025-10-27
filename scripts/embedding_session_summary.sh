6339fe3378b6564bd6beebc0960269ff484f1797
Mxqwthl <sc15933332964@gmail.com>
2025-10-27 23:47:24 +0800

    1

---
+++ scripts/embedding_session_summary.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Session summary generation script for Ubuntu/Linux
+
+# sets necessary environment variables and activates conda environment
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "$SCRIPT_DIR/env.sh"
+
+# gets session summaries using gpt-3.5-turbo and extract DRAGON embeddings for RAG database
+python task_eval/get_session_summary.py --data-file $DATA_FILE_PATH --out-file $OUT_DIR/$SESS_SUMM_OUTPUT_FILE \
+    --prompt-dir $PROMPT_DIR --emb-dir $EMB_DIR --use-date
